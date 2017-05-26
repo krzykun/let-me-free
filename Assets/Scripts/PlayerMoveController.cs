7b87ff577d8d9ae96106f97b4bac75c308f1a2a2
Krzysztof Kundera <krzykun358@student.polsl.pl>
2017-05-26 21:16:52 +0200

    first physics update: player and collision

--- Assets/Scripts/PlayerMoveController.cs
+++ Assets/Scripts/PlayerMoveController.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class PlayerMoveController : MonoBehaviour {
 
+	public float velCoeff = 0.0f;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * velCoeff;
+		var y = Input.GetAxis("Vertical") * Time.deltaTime * velCoeff;
+
+		//var x = Input.GetAxis("Horizontal") * 20.0f;
+		//var y = Input.GetAxis("Vertical") * 20.0f;
+
+		//if (x > )
+		transform.Translate(x, y, 0);
+		//tmp.AddForce(new Vector2(x, y), ForceMode2D.Impulse);
 	}
 }
