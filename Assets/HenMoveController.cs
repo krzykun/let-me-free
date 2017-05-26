d60e4630e6d88bb040e2ea8183f1be3095bb42b4
Krzysztof Kundera <krzykun358@student.polsl.pl>
2017-05-26 22:53:31 +0200

    hen changes direction when it hits a wall or other hen, player espawns when he hits a hen

--- Assets/HenMoveController.cs
+++ Assets/HenMoveController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class HenMoveController : MonoBehaviour {
@@ -7,7 +8,6 @@ public class HenMoveController : MonoBehaviour {
 	public Transform[] waypoints;
 	public bool goingLeft = false; // Tells us if the hen is going right (1) or left (0)
 	public float speed = 1.0f; // The amount given to Translate function on every update
-	private int counter = 0;
 	private SpriteRenderer rend;
 
 	// Use this for initialization
@@ -20,18 +20,20 @@ public class HenMoveController : MonoBehaviour {
 		speed *= -1;
 		goingLeft = !goingLeft;
 		rend.flipX = goingLeft;
+
 	}
 
 	// Update is called once per frame
 	void FixedUpdate () {
-		if (counter > 30)
+		transform.Translate(speed, 0, 0);
+	}
+
+	void OnCollisionEnter2D(Collision2D collObj)
+	{
+		if ((collObj.gameObject.tag == "wall") || (collObj.gameObject.tag == "hen"))
 		{
 			switchDirection();
-			counter = 0;
 		}
-		transform.Translate(speed, 0, 0);
-
-		counter++;
 	}
 	
 }
--- Assets/Scripts/PlayerMoveController.cs
+++ Assets/Scripts/PlayerMoveController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerMoveController : MonoBehaviour {
 
 	public float velCoeff = 1.0f;
+	public int health = 6;
+
 
 	// Use this for initialization
 	void Start () {
@@ -22,5 +24,19 @@ public class PlayerMoveController : MonoBehaviour {
 		//if (x > )
 		transform.Translate(x, y, 0);
 		//tmp.AddForce(new Vector2(x, y), ForceMode2D.Impulse);
+		if (health == 0)
+		{
+			health = 6; //TODO This is the defeat condition
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collObj)
+	{
+		if (collObj.gameObject.tag == "hen")
+		{
+			transform.position = new Vector2(-2.0f, 3.0f);
+			health--;
+			Debug.Log("PRZEGRYW" + health); //TODO lose life, reset position function
+		}
 	}
 }
