a649c3aa1ccc855a31ec0deef275cb8025329f6b
Krzysztof Kundera <krzykun358@student.polsl.pl>
2017-06-24 19:38:57 +0200

    small bugfixes

--- Assets/Scripts/PlayerMoveController.cs
+++ Assets/Scripts/PlayerMoveController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -22,6 +23,7 @@ public class PlayerMoveController : MonoBehaviour
     void Start ()
 	{
         JumpAnimation = this.GetComponent<Animation>();
+        OnPlayerDeath = GameOverTransition;
 	}
 
     // TODO This is triggered once the player reaches endPosition
@@ -113,7 +115,8 @@ public class PlayerMoveController : MonoBehaviour
                 //health = 6; //TODO This is the defeat condition
                 //GetComponent<SpriteRenderer>().sprite = PlayerSprites[health - 1];
             }
-			if (health <= 0) {
+            else
+			{
 				OnPlayerDeath ();
 			}
             return true;
