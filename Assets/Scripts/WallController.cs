52c42bbae92f0eb4ae49477d8966205205947d61
szymonjanota <szymekj18@gmail.com>
2017-06-24 15:56:43 +0200

    add wall controller, delete start and endpoint controllers

---
+++ Assets/Scripts/WallController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class WallController : MonoBehaviour {
+	private SpriteRenderer _renderer; 
+
+	// Use this for initialization
+	void Start () {
+		_renderer = GetComponent<SpriteRenderer> ();
+		BoxCollider2D collider = gameObject.AddComponent<BoxCollider2D> () as BoxCollider2D;
+		collider.size = _renderer.size;
+	}
+}
